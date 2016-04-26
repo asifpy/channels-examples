a4a5d7bbdf800698316841d15a90031a8b478215
Asif <saluasif@gmail.com>
2016-04-26 09:04:17 +0300

    wsgi fix

--- channels_examples/wsgi.py
+++ channels_examples/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for supplqn project.
+WSGI config for channels_examples project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "supplqn.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "channels_examples.settings")
 
 application = get_wsgi_application()
